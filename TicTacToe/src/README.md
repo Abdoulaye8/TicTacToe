7299fa715db4990b5e64411f605bdd7f6f1a43d5
abdoulaye soumare <abdoulaye.soumare@le-campus-numerique.fr>
2025-10-27 13:29:34 +0100

    correction puissance4

---
+++ TicTacToe/src/README.md
@@ -0,0 +1,63 @@
+```mermaid
+classDiagram
+direction LR
+    class BoardGame {
+	    - players : Player[]
+	    - currentPlayerIndex : int
+	    - size : int
+	    - view : View
+	    - interaction : InteractionUtilisateur
+	    + BoardGame(size, player1, player2, view, interaction)
+	    + play() : abstract
+	    + hasWinner() : abstract
+	    + isOver() : abstract
+	    # getCurrentPlayer() : Player
+	    # switchPlayer() : void
+    }
+
+    class TicTacToe {
+    }
+
+    class Gomoku {
+    }
+
+    class Puissance4 {
+    }
+
+    class Player {
+	    - symbol : char
+	    + Player(symbol)
+	    + getSymbol() : char
+    }
+
+    class ArtificialPlayer {
+	    + getMove() : int[]
+    }
+
+    class Cell {
+	    - owner : Player
+	    + isEmpty() : boolean
+	    + setOwner(player)
+	    + getSymbol() : char
+    }
+
+    class View {
+	    + afficherPlateau(board)
+    }
+
+    class InteractionUtilisateur {
+	    + demanderCoup(player) : int[]
+    }
+
+    class Main {
+	    + main(args)
+    }
+
+    BoardGame <|-- TicTacToe
+    BoardGame <|-- Gomoku
+    BoardGame <|-- Puissance4
+    ArtificialPlayer <|-- Player
+
+
+
+```
--- TicTacToe/src/games/Puissance4.java
+++ TicTacToe/src/games/Puissance4.java
@@ -7,29 +7,31 @@ import view.View;
 public class Puissance4 extends BoardGame {
 
     public static final int ALIGN_TO_WIN = 4;
+
     public Puissance4(Player player1, Player player2, View view, InteractionUtilisateur interaction) {
         super(6, 7, player1, player2, view, interaction);
     }
+
     @Override
     public void play() {
         while (!isOver()) {
             Player currentPlayer = getCurrentPlayer();
-            int[] move = interaction.demanderCoup(current);
+            int[] move = interaction.demanderCoup(currentPlayer);
             int col = move[1];
 
             int row = getAvailableRow(col);
             if (row != -1) {
-                applyMove(new int[] { row, col });
+                applyMove(new int[]{row, col});
                 view.printBoard(board);
                 switchPlayer();
-            } else  {
+            } else {
                 interaction.showMessage("colonne pleine, essayez un autre !");
             }
         }
         if (hasWinner()) {
             Player winner = players[(currentPlayerIndex + players.length - 1) % players.length];
             view.showMessage("victoire du jour " + winner + " !");
-        } else   {
+        } else {
             view.showMessage("match nul !");
         }
 
@@ -39,13 +41,20 @@ public class Puissance4 extends BoardGame {
     protected void applyMove(int[] move) {
         int col = move[0];
         int row = move[1];
-        getCell(row, col).setOwner(getCurrentPlayer());
-        incrementMoveCount();
+
+        if (isCellEmpty(row, col)) {
+            getCell(row, col).setOwner(getCurrentPlayer());
+            incrementMoveCount();
+        } else {
+            view.showMessage("colonne déjà occupé !");
+        }
 
     }
+
     private int getAvailableRow(int col) {
         for (int i = rows - 1; i >= 0; i--) {
-            if (isCellEmpty(i, col)) return i;
+            if (isCellEmpty(i, col))
+                return i;
         }
         return -1;
     }
@@ -53,31 +62,57 @@ public class Puissance4 extends BoardGame {
     @Override
     protected boolean hasWinner() {
         char symbol = getCurrentPlayer().getSymbol();
-        return checkAlignment(symbol, 4);
+        return checkLigne(symbol, ALIGN_TO_WIN);
     }
-    private boolean checkAlignment(char symbol, int toAlign) {
+
+    private boolean checkLigne(char symbol, int toAlign) {
         for (int i = 0; i < toAlign; i++) {
             for (int j = 0; j < toAlign; j++) {
                 if (board[i][j].getSymbol() == symbol) {
-                    if (checkDirection(i, j, 1, 0, symbol, toAlign) ||
-                        checkDirection(i, j, 0, 1, symbol, toAlign) ||
-                        checkDirection(i, j, 1, 1, symbol, toAlign)) ||
-                        checkDirection(i, j, 1, -1, symbol, toAlign))
-                    return true;
+                    if (checkCell(i, j, 1, 0, symbol, toAlign)) {
+                        System.out.println("align trouvé");
+                        return true;
+                    } else if (checkCell(i, j, 0, 1, symbol, toAlign)) {
+                        System.out.println("align horizontl trouvé");
+                        return true;
+                    } else if (checkCell(i, j, 1, 1, symbol, toAlign)) {
+                        System.out.println("align diagonal trouvé");
+                        return true;
+                    } else if (checkCell(i, j, 1, -1, symbol, toAlign)) {
+                        System.out.println("alignement diagonal trouvé");
+                        return true;
+                    }
+
                 }
             }
         }
+        return false;
     }
-private boolean checkDirection(int row, int col, int dRow, int dCol, char symbol, int toAlign) {
+
+
+    private boolean checkCell(int row, int col, int dRow, int dCol, char symbol, int toAlign) {
         for (int k = 0; k < toAlign; k++) {
             int r = row + dRow;
             int c = col + dCol;
-            if (r < 0 || r >= rows || c < 0 ||| c >= cols || board[r][c].getSymbol() != symbol)
+            if (r < 0 || r >= rows || c < 0 || c >= cols) {
+                return false;
+            } else if (board[r][c].getSymbol() != symbol) {
+                return true;
+            }
         }
+        return false;
 
-}
+    }
+
     @Override
     protected boolean isOver() {
-        return false;
+        if (hasWinner()) {
+            return true;
+        } else if (getMovesCount() >= rows * cols) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
